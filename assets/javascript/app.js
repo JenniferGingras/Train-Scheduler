46bafd2445e0cb7063c8e67635ed6d1ba6a4966a
Jennifer Gingras <jennifergingras@tests-MacBook-Pro-2.local>
2019-01-31 10:11:36 -0500

    set up database

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,44 +11,71 @@ $(document).ready(function () {
   };
   firebase.initializeApp(config);
 
-
+  // GLOBAL VARIABLES
   var database = firebase.database();
 
+  // SHOW CURRENT TIME
+  $("#current-time").text(moment().format('h:mm:ss a'));
 
+  // ATTACH ADDTRAIN FUNCTION TO SUBMIT BUTTON
+  $(".addInfoButton").on('click', addTrain);
 
-  // store user input into database
+  // FUNCTION TO STORE USER INPUT INTO DATABASE
   // --------------------------------
-  // attach event to form's submit button
-  $(".addInfoButton").on('click', function (event) {
-    console.log("I've been clicked!")
+  var addTrain = function () {
     // get info from specific elements in form
-    // var trainName = $(".train-name").val().trim();
-    // var trainDestination = $(".destination").val().trim();
-    // var trainTime = $(".train-time").val().trim();
-    // var trainFrequency = $(".frequency").val().trim();
+    var trainName = $("#train-name").val().trim();
+    var trainDestination = $("#destination").val().trim();
+    var trainStart = $("#train-start").val().trim();
+    var trainFrequency = $("#frequency").val().trim();
 
-    // // create object to store data
-    // var trainInfo = {
-    //   name: trainName,
-    //   destination: trainDestination,
-    //   time: trainTime,
-    //   frequency: trainFrequency,
-    // };
+    // create object to store data
+    var trainInfo = {
+      name: trainName,
+      destination: trainDestination,
+      start: trainStart,
+      frequency: trainFrequency,
+    };
 
-    // // put input into database
-    // database.ref().push(trainInfo);
+    // put input into database
+    database.ref("trains").push(trainInfo);
 
-    // // test with console log
+    // test with console log
     // console.log(trainInfo.name);
     // console.log(trainInfo.destination);
-    // console.log(trainInfo.time);
+    // console.log(trainInfo.start);
     // console.log(trainInfo.frequency);
-  });
+  };
 
-  // add user input to html
+  // ADD USER INPUT TO HTML
   // ------------------------
-  // make variables to hold the data that was just stored
-  // create new table rows that display the data
+  database.ref("trains").on("child_added", function (snapshot) {
+    // console.log(snapshot);
+    var a = snapshot.val();
+    // console.log(a);
+    //  make variables to hold the data that was just stored
+    var trainName = a.name;
+    var trainDestination = a.destination;
+    var trainStart = a.time;
+    var trainFrequency = a.frequency;
 
+    // calculate next train arrival
+    // current time minus when train starts running then divide by frequency = number of times train has run already
+    // number of times train has run, multiply frequency, add start time = previous arrival time
+    // previous arrival time plus frequency = next train arrival 
 
+    // calculate minutes until next arrival
+    // next train arrival minus current time = minutes until arrival
+
+    // create new table rows that display the data
+    var tableRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(trainDestination),
+      $("<td>").text(trainFrequency),
+      $("<td>").text(''),
+      $("<td>").text(''),
+    );
+
+    $(".trainTable").append(tableRow);
+  });
 })
